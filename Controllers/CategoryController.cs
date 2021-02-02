ee253e8c104af9274f8e75d395ca7ceb1145b530
Fernando Lucius <fernando.muramasa@gmail.com>
2021-02-02 20:19:56 -0300

    Primeiro commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace crudzAPI.Controllers
+{
+    [ApiController]
+    [Route("v1/categories")]
+    public class CategoryController : ControllerBase
+    {
+        
+    }
+
+}
